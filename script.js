52403cce784dc56f3b77d3e93b97ee1d645c51df
daseul <nldaseul@gmail.com>
2019-03-27 19:13:39 +0900

    Add image slide show feature

--- script.js
+++ script.js
@@ -17,6 +17,16 @@ req.onreadystatechange = function(){
 				// }
 				this.classList.toggle("image-selected");
 			}
+			div.onmouseover = function(){
+				var element = this;
+				this.timerid = setTimeout( function(){
+					element.classList.add("image-magnified");
+				}, 600 );
+			}
+			div.onmouseout = function(){
+				clearTimeout(this.timerid);
+				this.classList.remove("image-magnified");
+			}
 			var img = document.createElement("img");
 			img.src = data[i];
 			div.appendChild(img);
@@ -35,9 +45,29 @@ function selectAll(btn){
 			images[i].classList.remove("image-selected");
 		}
 	}
+
 	if( btn.value == "Select All" ){
 		btn.value = "Unselect All";
 	}else{
 		btn.value = "Select All";
 	}
+}
+
+function slideShow(btn){
+	// image-magnified 클래스를 순차적으로 추가하며 슬라이드쇼 효과를 낸다
+	var images = document.getElementsByClassName("image");
+	// 확대된 이미지를 축소하고 다음 이미지를 확대
+	var index = 0;
+	images[index].classList.add("image-magnified");
+
+	var intervalid = setInterval( function(){
+		images[index].classList.remove("image-magnified");
+		index += 1;
+		if( index < images.length ){
+			images[index].classList.add("image-magnified");
+		}
+		else{
+			clearInterval(intervalid);
+		}
+	}, 1000 );
 }
